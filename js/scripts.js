f3e4047e0543cfc1f3fbb7d0c9e72466a30cd9cf
Cody Fritz <clanalia55@gmail.com>
2020-06-09 10:51:57 -0700

    add html information and basic css styling, ready for js
    co-authored-by: Tre Sonna <tresonna@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+// User inputs sentence 
+// Sentence -> string
+// all VOWELS replaces with "-"
+// original string hidden
+// Example:
+// String Input: "Believe you can and you're halfway there. Theodore Roosevelt"
+// Puzzle Output: "B-l--v- y-- c-n -nd y--'r- h-lfw-y th-r-. Th--d-r- R--s-v-lt"
+
+// input -> array -> split -> replace vowel with - ->combine -> ??
